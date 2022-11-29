436d3ff3e624cdd00dfea57f71627d824e755bbf
mayomatias <72289719+mayomatias@users.noreply.github.com>
2022-11-29 11:50:08 -0300

    btn pdf

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// MENU DE HAMBURGUESA NAV BAR
 const hambBtn = document.getElementById('nav-hamburguer-menu');
 const menu = document.getElementById('menu');
 
@@ -10,3 +11,18 @@ const desplegarMenu = () => {
 }
 
 hambBtn.addEventListener('click',desplegarMenu);
+
+//ABOUT --> VER PDF BTN
+
+const aboutContainer = document.getElementById('about-img');
+const btn = document.getElementById('btn-pdf');
+const mostrarBtn = () => {
+        btn.style.display = 'block';
+}
+const ocultarBtn = () => {
+    btn.style.display = 'none';
+}
+
+aboutContainer.addEventListener('mouseover', mostrarBtn);
+btn.addEventListener('mouseover',mostrarBtn)
+aboutContainer.addEventListener('mouseleave', ocultarBtn);
